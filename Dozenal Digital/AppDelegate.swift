5e2b100a330a1af996d52e85b0b1f1bb978fc642
Neighborkid01 <60106973+Neighborkid01@users.noreply.github.com>
2020-01-30 14:15:30 -0500

    Initial Commit

---
+++ Dozenal Digital/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dozenal Digital
+//
+//  Created by admin on 1/30/20.
+//  Copyright © 2020 Caleb Roland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dozenal Digital/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dozenal Digital
+//
+//  Created by admin on 1/30/20.
+//  Copyright © 2020 Caleb Roland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
